e9cc04ff34c24ce5fcdb475be01403e1351786d6
Agustin <Aguspaccosi@hotmail.com>
2023-07-10 20:03:06 -0300

    Agregar archivos de proyecto.

---
+++ POO.Parcial.Consola/Program.cs
@@ -0,0 +1,81 @@
+﻿using POO.Parcial.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace POO.Parcial.Consola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int dia, mes, anio;
+            bool validar = true;
+
+            do
+            {
+                Console.WriteLine("Ingrese el dia: ");
+                if(int.TryParse(Console.ReadLine(), out dia))
+                { 
+                    validar = false;
+                }
+                else
+                {
+                    Console.WriteLine("El valor ingresado no es válido.");
+                }
+            } while (validar);
+            validar = true;
+
+            do
+            {
+                Console.WriteLine("Ingrese el mes: ");
+                if (int.TryParse(Console.ReadLine(), out mes))
+                {
+                    validar = false;
+                }
+                else
+                {
+                    Console.WriteLine("El valor ingresado no es válido.");
+                }
+
+            } while (validar);
+            validar = true;
+
+            do
+            {
+                Console.WriteLine("Ingrese el anios: ");
+                if (int.TryParse(Console.ReadLine(), out anio))
+                {
+                    validar = false;
+                }
+                else
+                {
+                    Console.WriteLine("El valor ingresado no es válido.");
+                }
+
+            } while (validar);
+
+            //Console.WriteLine("Ingrese el dia: ");
+            //int dia=int.Parse(Console.ReadLine());
+            //Console.WriteLine("Ingrese el mes: ");
+            //int mes = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Ingrese el anio: ");
+            //int anio = int.Parse(Console.ReadLine());
+
+            bool esFechaValida = Validador.Validar(dia, mes, anio);
+            if (esFechaValida)
+            {
+                Console.WriteLine("La fecha es válida.");
+            }
+            else
+            {
+                Console.WriteLine("La fecha no es válida.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ POO.Parcial.Entidades/Validador.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO.Parcial.Entidades
+{
+    public static class Validador
+    {
+        public static bool Validar(int dia, int mes, int anio)
+        {
+            if(anio<1 || mes>12 || dia < 1)
+            {
+                return false;
+            }
+            int maximodiasporMes = DiasPorMes(mes, anio);
+            return dia <= maximodiasporMes;
+        }
+
+        private static int DiasPorMes(int mes, int anio)
+        {
+            if(mes==1||mes==3||mes==5|| mes == 7 || mes == 8 || mes == 10|| mes == 12)
+            {
+                return 31;
+            }
+            else if(mes == 4 || mes == 6 || mes == 9 || mes == 11)
+            {
+                return 30;
+            }
+            else
+            {
+                if (EsBisiesto(anio))
+                {
+                    return 29;
+                }
+                else
+                {
+                    return 28;
+                }
+            }
+        }
+
+        private static bool EsBisiesto(int anio)
+        {
+            if (anio % 4 == 0)
+            {
+                if (anio % 100 == 0)
+                {
+                    if (anio % 400 == 0)
+                    {
+                        return true;
+                    }
+                    else { return false; }
+                }
+                else { return true; }
+            }
+            else { return false; }
+        }
+
+    }
+}
